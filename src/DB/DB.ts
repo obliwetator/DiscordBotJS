c02afd1f342d6b3740145108ed903582871c2331
root <patrykstyla22@gmail.com>
2020-06-09 20:16:26 +0100

    Added init implementation of storing messages

--- src/DB/DB.ts
+++ src/DB/DB.ts
@@ -1,35 +1,82 @@
-import { createConnection, Connection } from 'mysql'
+import { createConnection, Connection, FieldInfo, createPool, Pool } from 'mysql'
 import { host, username, password } from '../Config'
+import { TextChannel, Message } from 'discord.js'
+
+interface TablesInDiscordBot {
+    Tables_in_DiscordBot: string
+}
 
 class DB {
-    public con: Connection
+    public pool: Pool
     private DB_NAME: string = "DiscordBot"
-    private TablesList: string[] = []
-    private GetTables() {
-        // Get the tables in our DB
-        this.con.query(`show tables`,
-        (error, results, fields ) => {
-            if (error) throw error
-            results.forEach((element: any) => {
-                this.TablesList.push(element.Tables_in_DiscordBot)
-            });
-        })
-    }
-
+    private TablesList: Map<string,string> = new Map
     constructor() {
-        this.con = createConnection({
+        this.pool = createPool({
+            connectionLimit: 10,
             host: host,
             user: username,
             password: password,
             database: "DiscordBot"
         });
-        
-        this.con.connect(function (err) {
-            if (err) throw err;
-            console.log("Connected!");
-        });
 
         this.GetTables()
+    }
+
+    private SetQuery<T>(query: string, values: T) {
+        return new Promise((resolve, reject) => {
+            this.pool.query(query)
+        })
+    }
+    private GetQuery(query: string) {
+        return new Promise((resolve, reject) => {
+            this.pool.query(query, (error, results) => {
+                if (error) return reject(error)
+                return resolve(results)
+            })
+        })
+    }
+
+    public async CreateTable(name: TextChannel) {
+        let sql = "CREATE TABLE `DiscordBot`." + '`' + name.name + "_" + name.id +'`' + " ( `ID` INT NOT NULL AUTO_INCREMENT ,\
+         `message` VARCHAR(2000) NOT NULL ,\
+         `time_added` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,\
+         `user_id` VARCHAR(50) NOT NULL,\
+         `username` VARCHAR(50) NOT NULL,\
+         `discriminator` VARCHAR(4) NOT NULL,\
+         `bot` BOOLEAN NOT NULL,\
+          PRIMARY KEY (`ID`)) ENGINE = InnoDB;"
+
+          await this.GetQuery(sql)
+
+          this.TablesList.set(name.name + "_" + name.id, name.name + "_" + name.id)
+    }
+
+    public async RemoveTable(name: TextChannel) {
+        let sql = "DROP TABLE `DiscordBot`." + '`' + name.name + "_" + name.id + '`'
+        await this.GetQuery(sql)
+        this.TablesList.delete(name.name + "_" + name.id)
+    }
+
+    private async GetTables() {
+        let a = await this.GetQuery('show tables') as TablesInDiscordBot[]
+        a.forEach((value) => {
+            this.TablesList.set(value.Tables_in_DiscordBot,value.Tables_in_DiscordBot)
+        })
+    }
+
+    public GetTableList() {
+        return this.TablesList
+    }
+
+    public async AddLog(value: string) {
+        console.log(value)
+        let a = await this.GetQuery(`INSERT INTO log (message) VALUES ('${value}')`)
+    }
+
+    public async AddMessage(message: Message, channel: TextChannel) {
+        console.log((message.channel as TextChannel).name + ": " + message.content)
+        await this.GetQuery(`INSERT INTO \`${channel.name}_${channel.id}\` (message, user_id, username, discriminator, bot)
+        VALUES ('${message.content}', '${message.author.id}', '${message.author.username}', '${message.author.discriminator}', '${message.author.bot? 1: 0}')`)
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,55 @@ import { token } from './Config'
 import DB from './DB/DB'
 import {Client, GuildChannel, TextChannel} from 'discord.js'
 
-const database = new DB
+const database = new DB()
+
 const client = new Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user!.tag}!`);
+    database.AddLog(`Logged in as ${client.user!.tag}!`)
 });
 
 client.on('channelCreate', channel => {
-    if (channel.type == "text") {
-        channel = (channel as TextChannel)
-        console.log('Text Channel', TextChannel.name)
+    if (channel.type == 'text') {
+        let TextChannel = channel as TextChannel
+        database.CreateTable(TextChannel)
+        database.AddLog(`Text Channel Created:  ${TextChannel.name}`)
     }
-    console.log(channel)
+})
+
+client.on('channelDelete', channel => {
+    if (channel.type == 'text') {
+        let TextChannel = channel as TextChannel
+        database.RemoveTable(TextChannel)
+        database.AddLog(`Text Channel Deleted:  ${TextChannel.name}`)
+    }
 })
 
 client.on('message', msg => {
-    console.log((msg.channel as TextChannel).name + ": " + msg.content)
+    // Adds the database if it doesnt exist when typing into a channel
+    let list = database.GetTableList()
+    let counter = 0
+    list.forEach(element => {
+        if ((msg.channel as TextChannel).name + "_" + (msg.channel as TextChannel).id !== element) {
+            counter++
+        }
+        if (counter == list.size) {
+            console.log('counter', counter)
+            console.log('size', list.size)
+            database.CreateTable((msg.channel as TextChannel))
+        }
+    });
+    if (msg.channel.type == "text") {
+        // Has an attachment
+        if (msg.attachments.first()) {
+            // TODO: Handle attachments
+            database.AddMessage(msg, msg.channel)
+        }
+        else {
+            database.AddMessage(msg, msg.channel)
+        }
+    }
+
 });
-
-
 
 client.login(token);
