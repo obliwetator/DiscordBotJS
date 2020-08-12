d69df566d78c4866eb64dbdff8b1a38cd6797812
root <root@WS295203-vm.remote.ac>
2020-08-12 22:26:54 +0000

    Moved server to a separate process

--- src/WebSocketServer/Server.ts
+++ src/WebSocketServer/Server.ts
@@ -1,42 +1,56 @@
-import WebSocket from 'ws';
-import fs from 'fs';
-import https from 'https';
+import ws from "ws";
+import fs from "fs";
+import https from "https";
 
 export const server = https.createServer({
-    cert: fs.readFileSync('/etc/letsencrypt/live/patrykstyla.com/fullchain.pem'),
-    key: fs.readFileSync('/etc/letsencrypt/live/patrykstyla.com/privkey.pem')
+	cert: fs.readFileSync("/etc/letsencrypt/live/patrykstyla.com/fullchain.pem"),
+	key: fs.readFileSync("/etc/letsencrypt/live/patrykstyla.com/privkey.pem"),
 });
 
 // const wss = new WebSocket.Server({ server ,path:"/ws"});
-const wss = new WebSocket.Server({ server }).on('listening', () => {
-    console.log('WS Server is ready');
-});
+const wss = new ws.Server({ server }).on(
+	"listening",
+	() => {
+		console.log("WS Server is ready");
+	},
+);
 
-wss.on('error', (error) => {
-    console.log('Error', error)
-})
-wss.on('connection', function connection(ws, req) {
-    // const id = setInterval(function () {
-    //     ws.send(JSON.stringify(process.memoryUsage()), function () {
-    //         //
-    //         // Ignore errors.
-    //         //
-    //     });
-    // }, 100);
+wss.on(
+	"error",
+	(error) => {
+		console.log("Error", error);
+	},
+);
+wss.on(
+	"connection",
+	(ws) => {
+		// const id = setInterval(function () {
+		//     ws.send(JSON.stringify(process.memoryUsage()), function () {
+		//         //
+		//         // Ignore errors.
+		//         //
+		//     });
+		// }, 100);
+		ws.on(
+			"close",
+			(a,b) => {
+				console.log(`Client ${a} dissconnetced because ${b}`);
+				// clearInterval(id);
+			},
+		);
 
-    ws.on('close', function () {
-        console.log('Server is closed');
-        // clearInterval(id);
-    });
-
-    ws.on('message', function incoming(message) {
-        console.log('Server received: %s', message);
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-              client.send(message);
-            }
-          });
-    });
-});
+		ws.on(
+			"message",
+			(message) => {
+				console.log("Server received: %s", message);
+				wss.clients.forEach((client) => {
+					if (client !== ws && client.readyState === WebSocket.OPEN) {
+						client.send(message);
+					}
+				});
+			},
+		);
+	},
+);
 
 server.listen(8080);
