e5212054fee89d039398ecdb1728872b9e1b2d4a
root <root@WS295203-vm.remote.ac>
2020-08-17 23:17:29 +0000

    VoiceStates fully implemented

--- src/HandleVoiceState.ts
+++ src/HandleVoiceState.ts
@@ -1,36 +1,107 @@
-import { VoiceState } from "discord.js"
+import { VoiceState, User } from "discord.js"
+import DB from "./DB/DB"
 
 /**
  * Decides what was the action in the users voice state\
  * muted/defean... etc
  */
-export function HandleVoiceState(oldState: VoiceState, newState: VoiceState) {
-    if (newState.mute || newState.deaf) {
-        // User was muted/deafened
-        if (newState.selfMute) {
-            console.log('User self-muted')	
-        } else if (newState.serverMute) {
-            console.log('User server-muted')
-        }
+export async function HandleVoiceState(oldState: VoiceState, newState: VoiceState, database: DB) {
+	// User is muted/defeaned in any way
+	if ((!oldState.selfDeaf && newState.selfDeaf) ||
+		(!oldState.selfMute && newState.selfMute) ||
+		(!oldState.serverDeaf && newState.serverDeaf) ||
+		(!oldState.serverMute && newState.serverMute)) {
+		// User was muted
+		if (newState.selfMute && !oldState.selfMute) {
+			database.AddVoiceState(EnumVoiceState.mute, newState.id, newState.channelID!)
+		} else if (newState.serverMute && !oldState.serverMute) {
+			database.AddVoiceState(EnumVoiceState.server_mute, newState.id, newState.channelID!, await HandleVoiceStateExecutor(newState))
+		}
+		// defeaned
+		if (newState.selfDeaf && !oldState.selfDeaf) {
+			database.AddVoiceState(EnumVoiceState.deaf, newState.id, newState.channelID!)
+		} else if (newState.serverDeaf && !oldState.serverDeaf) {
+			database.AddVoiceState(EnumVoiceState.server_deaf, newState.id, newState.channelID!, await HandleVoiceStateExecutor(newState))
+		}
 
-        if (newState.selfDeaf) {
-            console.log('User self-deafened')
-        } else if (newState.serverDeaf) {
-            console.log('User server-deafened')
-        }
+		return
+	} else if ((oldState.selfDeaf && !newState.selfDeaf) ||
+		(oldState.selfMute && !newState.selfMute) ||
+		(oldState.serverDeaf && !newState.serverDeaf) ||
+		(oldState.serverMute && !newState.serverMute)) {
+		// user was unmuted
+		if (!newState.selfMute && oldState.selfMute) {
+			database.AddVoiceState(EnumVoiceState.unmute, newState.id, newState.channelID!)
+		} else if (!newState.serverMute && oldState.serverMute) {
+			database.AddVoiceState(EnumVoiceState.server_unmute, newState.id, newState.channelID!, await HandleVoiceStateExecutor(newState))
+		}
+		// undeafened
+		if (!newState.selfDeaf && oldState.selfDeaf) {
+			database.AddVoiceState(EnumVoiceState.undeafen, newState.id, newState.channelID!)
+		} else if (!newState.serverDeaf && oldState.serverDeaf) {
+			database.AddVoiceState(EnumVoiceState.server_undeafen, newState.id, newState.channelID!, await HandleVoiceStateExecutor(newState))
+		}
 
-    } else if (!newState.mute || !newState.deaf) {
-        // user was unmuted/undeafened
-        if (!newState.selfMute && oldState.selfMute) {
-            console.log('User un-self-muted')
-        } else if (!newState.serverMute && oldState.serverMute) {
-            console.log('User un-server-muted')
-        }
+		return
+	}
 
-        if (!newState.selfDeaf && oldState.selfDeaf) {
-            console.log('User un-self-deafened')
-        } else if (!newState.serverDeaf && oldState.serverDeaf) {
-            console.log('User un-server-deafened')
-        }
-    }
+	if (newState.selfVideo && !oldState.selfVideo) {
+		// User turned on webcam
+		database.AddVoiceState(EnumVoiceState.video_start, newState.id, newState.channelID!)
+		return;
+	}
+	else if (!newState.selfVideo && oldState.selfVideo) {
+		// User turned off camera
+		database.AddVoiceState(EnumVoiceState.video_stop, newState.id, newState.channelID!)
+		return;
+	}
+
+	if (newState.streaming && !oldState.streaming) {
+		// User started streaming
+		database.AddVoiceState(EnumVoiceState.streaming_start, newState.id, newState.channelID!)
+		return;
+	}
+	else if (!oldState.streaming && oldState.streaming) {
+		// User stopped streaming
+		database.AddVoiceState(EnumVoiceState.streaming_stop, newState.id, newState.channelID!)
+		return;
+	}
+}
+
+async function HandleVoiceStateExecutor(newState: VoiceState): Promise<string> {
+	const fetchedLogs = await newState.guild.fetchAuditLogs({
+		limit: 1,
+		type: 'MEMBER_UPDATE',
+	});
+
+	const UpdateLog = fetchedLogs.entries.first();
+
+	if (!UpdateLog)
+		return "";
+
+	const { executor, target } = UpdateLog;
+
+	// Check if the log was for that user
+	if ((target as User).id === newState.id) {
+		return executor.id
+	} else {
+		return ""
+	}
+}
+
+export enum EnumVoiceState {
+	mute,
+	unmute,
+	deaf,
+	undeafen,
+	server_mute,
+	server_unmute,
+	server_deaf,
+	server_undeafen,
+	video_start,
+	video_stop,
+	streaming_start,
+	streaming_stop,
+	channel_join,
+	channel_leave,
 }
