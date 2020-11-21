994571a2e260d13d367418ac7a6d088eaf55c37e
Patryk <43710236+PatrykStyla@users.noreply.github.com>
2020-11-21 02:57:02 +0000

    Much more efficient message decoding. Remove old websocket code

--- src/WebSocketServer/Server.ts
+++ src/WebSocketServer/Server.ts
@@ -8,7 +8,6 @@ import uuid from 'uuid-random';
 import { DEDICATED_COMPRESSOR_3KB, SHARED_COMPRESSOR, SSLApp } from "uWebSockets.js";
 import { Message } from "discord.js";
 
-const enc = new TextDecoder("utf-8");
 const ChannelIDRegex = /(id) (\w{16,20})/
 const RegexIsMessage = /message/
 
@@ -35,7 +34,7 @@ SSLApp({
 		)
 	},
 	open: (ws) => {
-		if (BotIPV6Adress.has(enc.decode(ws.getRemoteAddressAsText()))) {
+		if (BotIPV6Adress.has(Buffer.from(ws.getRemoteAddressAsText()).toString())) {
 			// console.log(ctx.blueBright("Bot connected"))
 			console.log("bot connected")
 
@@ -48,7 +47,7 @@ SSLApp({
 		if(!message.byteLength){
 			return
 		}
-		const DecMessage = enc.decode(message);
+		const DecMessage = Buffer.from(message).toString()
 
 		if (ws.bot) {
 			const JsonMessage = JSON.parse(DecMessage)
@@ -78,82 +77,6 @@ SSLApp({
 		console.log('Listening to port 9001');
 	}
 })
-// export const server = https.createServer({
-// 	cert: fs.readFileSync("/etc/letsencrypt/live/patrykstyla.com/fullchain.pem"),
-// 	key: fs.readFileSync("/etc/letsencrypt/live/patrykstyla.com/privkey.pem"),
-// });
-
-// // const wss = new WebSocket.Server({ server ,path:"/ws"});
-// const wss = new ws.Server({ server })
-
-
-// wss.on("error", (error) => {
-// 	console.log("Error", error);
-// });
-
-// wss.on("headers", (headers, request) => {
-
-// })
-
-// wss.on("close", () => {
-
-// })
-
-// wss.on("listening", () => {
-// 	console.log("WS Server is ready");
-// })
-
-// function noop() { }
-
-// wss.on("connection", (ws, request) => {
-
-// 	(ws as any).isAlive = true;
-// 	// const id = setInterval(function () {
-// 	//     ws.send(JSON.stringify(process.memoryUsage()), function () {
-// 	//         //
-// 	//         // Ignore errors.
-// 	//         //
-// 	//     });
-// 	// }, 100);
-// 	ws.on("pong", (data) => {
-// 		(ws as any).isAlive = true
-// 	})
-
-// 	ws.on("ping", (data) => {
-
-// 	})
-
-// 	const interval = setInterval(function ping() {
-// 		wss.clients.forEach(function each(ws) {
-// 			if ((ws as any).isAlive === false) {
-// 				console.log(ctx.red(`Websocket terminated due to innactivity: ${ws}`))
-// 				return ws.terminate();
-// 			}
-// 			(ws as any).isAlive = false;
-// 			ws.ping(noop);
-// 		});
-// 	}, 30000);
-
-// 	ws.on("error", (error) => {
-// 		console.log(`WS server error: ${error}`)
-// 	})
-// 	ws.on("close", (a, b) => {
-// 		console.log(`Client ${a} dissconnetced because ${b}`);
-// 		// clearInterval(id);
-// 		clearInterval(interval);
-// 	});
-
-// 	ws.on("message", (message) => {
-// 		console.log("Server received: %s", message);
-// 		wss.clients.forEach((client) => {
-// 			if (client !== ws && client.readyState === ws.OPEN) {
-// 				client.send(message);
-// 			}
-// 		});
-// 	});
-// });
-
-// server.listen(8080);
 
 
 interface IBotMessage {
